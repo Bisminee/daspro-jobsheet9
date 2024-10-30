c122ec448d65181adaf301eb0c6966ee43f9fe3b
Bisminee <fiksilmiah@gmail.com>
2024-10-30 09:46:05 +0700

    Array Pertama

--- SearchNilai07.java
+++ SearchNilai07.java
@@ -1,19 +1,37 @@
+
+import java.util.Scanner;
+
 public class SearchNilai07 {
     public static void main(String[] args) {
-        int arrNilai[] = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        int key, hasil = 0, jumlahInput;
+
+        System.out.print("Masukkan banyaknya nilai yang ingin dimmasukkan: ");
+        jumlahInput = sc.nextInt();
+
+        int arrNilai[] = new int[jumlahInput];
+        for(int i = 0; i < arrNilai.length; i++){
+            System.out.print("Masukkan nilai ke-" + (i+1) + " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key: ");
+        key = sc.nextInt();
 
 
         for(int i = 0; i < arrNilai.length; i++){
             if(key == arrNilai[i]){
                 hasil = i;
+                System.out.println();
+                System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+                System.out.println();
+                break;
+            }else{
+                System.out.println("\nNilai tidak dapat ditemukan\n");
                 break;
             }
         }
 
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        
     }
 }
