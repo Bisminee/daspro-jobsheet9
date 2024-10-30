e749e04b45fc681863b032d1f8b8abbe5cf16631
Bisminee <fiksilmiah@gmail.com>
2024-10-30 09:20:18 +0700

    Array Pertama

--- ArrayNilaiRata07.java
+++ ArrayNilaiRata07.java
@@ -2,18 +2,27 @@ import java.util.Scanner;
 public class ArrayNilaiRata07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nilaiMhs[] = new int[10];
+        int jumlahMhs;
+        int mhsLulus = 0;
         double rata2, total = 0;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jumlahMhs = sc.nextInt();
+        int nilaiMhs[] = new int[jumlahMhs];
 
         for(int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70){
+                mhsLulus++;
+            }
         }
         for(int i = 0; i < nilaiMhs.length; i++){
             total += nilaiMhs[i];
         }
 
         rata2 = total/nilaiMhs.length;
+        System.out.println("\nJumlah mahasiswa lulus = " + mhsLulus + " mahasiswa");
         System.out.println("Rata-rata nilai = " + rata2);
     }
 }
